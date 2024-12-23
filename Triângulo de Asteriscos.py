1cf6eb89c5298d5f66c712f1a724276a25aa1bf5
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2024-12-23 14:28:09 -0300

    Triângulo de Asteriscos.py

--- Triângulo de Asteriscos.py
+++ Triângulo de Asteriscos.py
@@ -1,7 +1,15 @@
-# Número de linhas do triângulo
-linhas = int(input("Digite o número de linhas do triângulo: "))
+def exibir_triangulo(n):
+    for i in range(1, n + 1):
+        # Imprime espaços iniciais para alinhar os asteriscos
+        print(' ' * (n - i), end='')
 
-# Loop para construir o triângulo
-for i in range(1, linhas + 1):
-    print('*' * i)
+        # Imprime os números da linha
+        for j in range(1, i + 1):
+            print("*", end=' ')
+
+        print()  # Nova linha após cada linha do triângulo
+
+# Solicita o número de linhas ao usuário
+n = int(input("Digite o número de linhas para o triângulo de asteriscos: "))
+exibir_triangulo(n)
 input()
